9cd5a778bd03ef89345de481d32ba8311cd837a7
BugleJones <spencerjhamilton@gmail.com>
2017-08-18 05:02:04 +0000

    Multiple exercises added, arrays, conditionals and functions

--- hello.js
+++ hello.js
@@ -1,5 +1,9 @@
+var cheeseType = ["brie", "gouda", "cheddar"];
+
+
+
 function sayHello(name) {
-  console.log("Hello, " + name)
+  console.log("Hello, " + name + ", I am a computer, you are not, also my favourite cheese is " + cheeseType[1]);
 }
 
 sayHello("Marimba");
